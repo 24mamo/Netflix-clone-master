74562eed1a1d2ae54aacce8f2ff79a35856826d2
anbe-Tech <anberberm@ymail.com>
2021-03-19 00:10:14 -0500

    Initial commit

---
+++ build/precache-manifest.e3e9a739a9710a2d96ae62e46b9ed1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd87db0ad6654cd7d043b4823c310db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b896e6c4448051ce5dae",
+    "url": "/static/css/main.b215b06a.chunk.css"
+  },
+  {
+    "revision": "92f2502db03248df14ab",
+    "url": "/static/js/2.7199c33c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.7199c33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b896e6c4448051ce5dae",
+    "url": "/static/js/main.e4cb6dc2.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  }
+]);
